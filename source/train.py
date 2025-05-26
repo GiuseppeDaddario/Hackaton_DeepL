70d2b6429a7ef5375ac659e60e88e25fde28e3a8
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-05-26 09:49:45 +0200

    (fix) epoch boost

--- source/train.py
+++ source/train.py
@@ -58,7 +58,7 @@ def train(
         elif criterion_type == "gcod":
             if current_epoch <= epoch_boost:
                 optimizer_model.zero_grad()
-                loss_ce = loss_function_obj(output_logits, true_labels_int)
+                loss_ce = torch.nn.CrossEntropyLoss(output_logits, true_labels_int)
                 loss_ce.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer_model.step()
